62c6165739d1fdc0713775efa5def65542a38128
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2022-12-27 14:25:23 +0500

    Task Completed

--- Productbox/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/user");
-
-router.post("/created", controller.userCreated);
-router.post("/generateOtp", controller.generateOtp);
-router.get("/:userId/verifyOTP", controller.verifyOtp);
-
-module.exports = router;
